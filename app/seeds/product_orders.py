4608499989edcb7af9b06a197b8bbfeab80c274f
Nur Unlu <ecz.ilknurunlu@gmail.com>
2024-04-24 17:59:10 -0700

    update

--- app/seeds/product_orders.py
+++ app/seeds/product_orders.py
@@ -12,8 +12,8 @@ def seed_product_orders():
         product_order6=ProductOrder(product_id=1, order_id=3, quantity=2)
         product_order7=ProductOrder(product_id=2, order_id=3, quantity=1)
         product_order8=ProductOrder(product_id=3, order_id=3, quantity=1)
-        product_order9=ProductOrder(product_id=4, order_id=4, quantity=1)
-        product_order10=ProductOrder(product_id=5, order_id=5, quantity=1)
+        product_order9=ProductOrder(product_id=4, order_id=3, quantity=1)
+        product_order10=ProductOrder(product_id=5, order_id=2, quantity=1)
 
 
         all_product_orders = [product_order1, product_order2, product_order3,product_order4,product_order5,product_order6,product_order7,product_order8,product_order9,product_order10]
